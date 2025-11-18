44d9e00e83316528ec0033492229d5bf99eba041
Mayra-bio <mbarcenas37@alumnos.uaq.mx>
2025-11-17 21:32:57 -0600

    Update Proyecto_bioinfo.R

--- Proyecto_bioinfo.R
+++ Proyecto_bioinfo.R
@@ -3,3 +3,6 @@
 #Datos 
 
 alimentos <- c("tacos", "birria", "pizza", "hamburguesas", "hotdog", "moronga", "tacos sudados","barbacoa", "carnitas" )
+
+temperatura <- c(37, 36, 24, 36, 33, 37, 37, 37, 36)
+horas_de_exposicion <- c(0.2, 1, 3, 0.2, 0.1, 2, 3, 3, 4)
