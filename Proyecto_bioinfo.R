f250437562214938a7f19332b835383fe258dabc
celi-micro <cmartinez184@alumnos.uaq.mx>
2025-11-17 21:19:38 -0600

    se subio

---
+++ Proyecto_bioinfo.R
@@ -0,0 +1,5 @@
+####Modelo predictivo de riesgo de Contaminacion en Alimentos Callejeros###
+
+#Datos 
+
+alimentos <- c("tacos", "birria", "pizza", "hamburguesas", "hotdog", "moronga", "tacos sudados","barbacoa", "carnitas" )
