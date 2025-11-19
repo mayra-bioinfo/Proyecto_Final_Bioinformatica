7487e8fb50eb7bea0ef5893cbc4d7ee005672547
Mayra-bio <mbarcenas37@alumnos.uaq.mx>
2025-11-18 18:36:38 -0600

    Variables
    añadi las variables que podemos considerar para el codigo

--- Proyecto_bioinfo.R
+++ Proyecto_bioinfo.R
@@ -2,11 +2,19 @@
 
 #Datos 
 
-alimentos <- c("tacos", "birria", "pizza", "hamburguesas", "hotdog", "moronga", "tacos sudados","barbacoa", "carnitas" )
+alimentos <- readline (prompt = "introduce el tipo de alimento que deseas analizar: ")
+temperatura <- readline (prompt = "introduce la temperatura a la que se encuentra el alimento: ")
+exposicion <- readline (prompt = "cuanto tiempo ha estado expuesto el alimento?: ")
+manipulacion <- readline (prompt = "contesta SI o NO si la comida ha sido manipulada con las manos: ")
+tiempo <- readline (prompt = "en que estacion del año te encuentras?: ")
 
-temperatura <- c(37, 36, 24, 36, 33, 37, 37, 37, 36)
+
+
+alimentos <- c("tacos", "birria", "pizza", "hamburguesas", "hotdog", "moronga", "tacos sudados", "barbacoa", "carnitas", "tortas")
+temperatura <- c(37, 36, 35, 36, 33, 37, 37, 37, 36, 35)
 horas_de_exposicion <- c(0.2, 1, 3, 0.2, 0.1, 2, 3, 3, 4)
+bacterias_comunes <- c("Escherichia coli", "Shigella spp", "Salmonella typhi", "Staphylpcoccus aureus", "Campylobacter spp", "Listeria monocytogenes", "Klebsiella spp")
+manipulacion <- c("si", "si", "no", "si", "si", "no", "si", "si", "si")
+estacion_año <- c()
 
 
-
-bacterias_comunes <- c("E.coli", "Shigella", "Salmonella", "Staphylpcoccus aureus", "Campylobacter", "Listeria monocytogenes", "Klebsiella")
